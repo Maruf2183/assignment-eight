bb87dc983130992d0fd607b641515ca950ecb3a2
Maruf Raju <Marufraju2183@gmail.com>
2021-09-26 01:06:41 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <img style={{ height:'200px',width:'200px',borderRadius:'50px',marginRight:'30px'}} src="https://images.gr-assets.com/authors/1456905354p8/15050617.jpg" alt="" />
+      <img style={{ width: '200px', borderRadius: '50px' }} src="https://www.zulkarnine.com/wp-content/uploads/2020/07/PP.jpg" alt="" />
+      <img src="https://www.theindependentbd.com/assets/news_images/zaheed-google-m.jpg" alt="" />
+
+    </div>
+  );
+}
+
+export default App;
